b5e16e0b52990fb1537e4f10b5b4923de4945766
adithyansraj <adithyan.raj@tortil.com>
2024-07-16 17:31:08 +0530

    basic redux

---
+++ Section 19/01-starting-project/src/App.js
@@ -0,0 +1,13 @@
+import { Counter } from "./components/Counter";
+import CounterClass from "./components/Counter";
+
+function App() {
+  return (
+    <>
+      <Counter />
+      <CounterClass />
+    </>
+  );
+}
+
+export default App;
