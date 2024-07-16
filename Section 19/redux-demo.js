9be551d12ba3cca4dce3bffa4316087e6e66e69a
adithyansraj <adithyan.raj@tortil.com>
2024-07-16 15:39:56 +0530

    redux in js

--- Section 19/redux-demo.js
+++ Section 19/redux-demo.js
@@ -1,5 +1,31 @@
 const redux = require("redux");
 
-const counterReducer = () => {};
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
 
-const store = redux.createStore();
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+const store = redux.createStore(counterReducer);
+
+// console.log(store.getState());
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
